7751690fa518730fa5e77ec72f567cd533c4b474
RPDavlyatshin <48312008+RPDavlyatshin@users.noreply.github.com>
2021-01-11 20:52:31 +0500

    upd

--- FEM/FEM.cpp
+++ FEM/FEM.cpp
@@ -1,6 +1,14 @@
 ﻿#include <iostream>
 #include <vector>
 
+#include <iostream>
+#include <fstream>
+#include <string>
+//Строка с количеством узлов
+//Список узлов x,y,z
+//Строка с количеством элементов
+//Список элементов
+
 #include "GlobalData.h"
 #include "Node.h"
 
@@ -9,5 +17,33 @@ int main()
     GlobalData();
     std::vector<Node> nodes(GlobalData::count_nodes);
     //std::
+
+    int main()
+    {
+        int NodeNumber, FiniteElementNumber;
+
+        std::string inputFile, line;
+        inputFile = "temp_model.k";
+
+        std::ifstream in(inputFile);
+
+        if (in.is_open())
+        {
+            in >> NodeNumber;
+            Node* Nodes = new Node[NodeNumber];
+            for (int i = 0; i < NodeNumber; i++)
+            {
+                in >> Nodes[i].id >> Nodes[i].x >> Nodes[i].y >> Nodes[i].z;
+            }
+
+            in >> FiniteElementNumber;
+            FiniteElement* FiniteElements = new FiniteElement[FiniteElementNumber];
+            for (int i = 0; i < FiniteElementNumber; i++)
+            {
+                in >> FiniteElements[i].id >> FiniteElements[i].
+            }
+        }
+        in.close();
+    }
 
 }
