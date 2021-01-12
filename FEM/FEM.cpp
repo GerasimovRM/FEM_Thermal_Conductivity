63da9fc9ccd553429303e5ac8600a11062677936
Roman M. Gerasimov <romagrizly@gmail.com>
2021-01-12 15:44:52 +0500

    fem update

--- FEM/FEM.cpp
+++ FEM/FEM.cpp
@@ -17,5 +17,15 @@ int main()
     DataLoader::load_nodes_and_elements("data.txt", Nodes, FiniteElements);
     Node::PrintNodes(Nodes);
     FiniteElement::PrintElements(FiniteElements);
-    //Grid grid(FiniteElements);
+    Grid grid(FiniteElements);
+    for (auto element : grid.elements)
+    {
+        element.calculate_local_C();
+        element.calculate_local_K();
+        element.calculate_local_f();
+    }
+    grid.calculate_global_C();
+    grid.calculate_global_K();
+    grid.calculate_global_f();
+    Common::matrix_print(Common::vector_to_array(grid.global_C), grid.global_C.size(), grid.global_C[0].size());
 }
